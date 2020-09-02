c9e2f0061edf60339f847608d32ecc014e015028
miile7 <miile7@gmx.de>
2020-09-02 09:24:28 +0200

    Adding Version hints for 1.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="execdmscript",
-    version="1.0.1",
+    version="1.0.2",
     author="miile7",
     author_email="miile7@gmx.de",
     description=("A python module for executing DM-Script from python in the " + 
